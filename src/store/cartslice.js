1cae6744d25041e323258fd365af17bcbaf53c64
aksh vaishnav <vaishnavaksh@gmail.com>
2024-10-08 17:18:23 +0530

    comasfd

--- src/store/cartslice.js
+++ src/store/cartslice.js
@@ -1,36 +1,19 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-export const fetchContent = createAsyncThunk(
-  'content/fetchContent',
-  async () => {
-    const res = await axios('https://jsonplaceholder.typicode.com/photos')
-    const data = await res.data
-    return data
-  }
-)
+
+
 
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     cart:[] 
+
   },
   reducers: {
     
   },
-  extraReducers: (builder) => {
-    builder.addCase(fetchContent.pending, (state) => {
-      state.isLoading = true
-    })
-    builder.addCase(fetchContent.fulfilled, (state, action) => {
-      state.isLoading = false
-      state.contents = action.payload
-    })
-    builder.addCase(fetchContent.rejected, (state, action) => {
-      state.isLoading = false
-      state.error = action.error.message
-    })
-  },
+
   
 })
 
