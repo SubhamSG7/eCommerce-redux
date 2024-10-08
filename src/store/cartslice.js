5038052de35cc8b6b606c4322e08466346ddb66e
aksh vaishnav <vaishnavaksh@gmail.com>
2024-10-08 17:17:37 +0530

    user slicr added

--- src/store/cartslice.js
+++ src/store/cartslice.js
@@ -1,5 +1,15 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit'
 
+export const fetchContent = createAsyncThunk(
+  'content/fetchContent',
+  async () => {
+    const res = await axios('https://jsonplaceholder.typicode.com/photos')
+    const data = await res.data
+    return data
+  }
+)
+
+
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
@@ -7,6 +17,19 @@ const cartSlice = createSlice({
   },
   reducers: {
     
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchContent.pending, (state) => {
+      state.isLoading = true
+    })
+    builder.addCase(fetchContent.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.contents = action.payload
+    })
+    builder.addCase(fetchContent.rejected, (state, action) => {
+      state.isLoading = false
+      state.error = action.error.message
+    })
   },
   
 })
