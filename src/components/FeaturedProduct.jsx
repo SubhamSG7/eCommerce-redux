fc059f3e0da415ee18df95447d1f7e1f2cd8c889
Muskan0423 <muskankewlani123@.com>
2024-10-09 10:39:38 +0530

    changes for home and products

--- src/components/FeaturedProduct.jsx
+++ src/components/FeaturedProduct.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 const FeaturedProduct = () => {
   return (
 <>
-<div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+<div className="w-fit px-2 mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
 
    <Card/>
   
