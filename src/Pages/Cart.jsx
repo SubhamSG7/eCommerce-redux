1a2ebc3d993a3ea23d55f5d75a2348e2cb3ec64e
Muskan0423 <muskankewlani123@.com>
2024-10-09 16:34:38 +0530

    changes for cart

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -4,7 +4,7 @@ import Card from '../components/Card';
 
 export default function Cart() {
   const cartItems = useSelector((state) => state.cart.cartData);
-  const total = cartItems.reduce((sum, item) => sum + item.price, 0); // Assuming `price` is a field in your item
+  const total = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0); 
 
   return (
     <>
@@ -20,19 +20,20 @@ export default function Cart() {
             <div className="flex-1">
               <div className="flex flex-wrap md:flex-row">
                 {cartItems.map((item) => (
-                  <Card key={item.id} item={item} />
+                 
+                    <Card item={item} />
+                
                 ))}
               </div>
             </div>
             <div className="md:w-1/3 md:ml-4 mt-4 md:mt-0">
               <div className="bg-gray-100 p-4 text-black rounded-lg shadow">
                 <h3 className="text-xl font-bold mb-4">Product Summary</h3>
-                <ul>
-                 
-                </ul>
+               
                 <div className="border-t mt-4 pt-2">
-                  <h3 className="text-xl font-bold">Total: ${total.toFixed(2)}</h3>
+                  <h3 className="text-xl font-bold">Total: {total.toFixed(2)}</h3>
                 </div>
+                <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded">Checkout</button>
               </div>
             </div>
           </div>
