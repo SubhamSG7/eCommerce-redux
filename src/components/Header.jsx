869a3953d9ff65644df4ce3ec23148a754fa0170
Muskan0423 <muskankewlani123@.com>
2024-10-10 10:41:07 +0530

    changes for slider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -100,54 +100,42 @@ dispatch(currencyChange(e.target.value));
 
   return (
 
-    <div className='bg-cyan-500 flex justify-center items-center h-20 gap-2 max-w-full mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-white'><Link to="/">Ecommerce</Link></h1>
-
-      <ul className='flex justify-center items-center'>
+    <div className=' bg-gradient-to-t from-slate-500 to-slate-800 flex justify-between items-center h-20 max-w-full mx-auto px-4 text-white'>
+    <h1 className='text-3xl font-bold'>
+      <Link to="/">Ecommerce</Link>
+    </h1>
+  
+    <div className='flex items-center space-x-4 ml-auto'> 
+      <ul className='flex items-center space-x-4'> 
         {navItems.map(item => (
-          <li
-            key={item.id}
-            onClick={() => handleItemClick(item.nav)}
-            className='p-5 hover:bg-white rounded-xl  cursor-pointer duration-300 hover:text-black'
-          >
-            {item.text}
-          </li>
+          <Link to={item.nav} key={item.id}> {/* Moved key to Link */}
+            <li
+              className='p-2 hover:bg-white rounded-xl cursor-pointer duration-300 hover:text-black'
+            >
+              {item.text}
+            </li>
+          </Link>
         ))}
       </ul>
+  
       <div onClick={handleNav} className='block md:hidden'>
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-
-      <ul
-        className={
-          nav
-            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
-            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
-        }
-      >
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Ecommerce</h1>
-
-        {navItems.map(item => (
-          <li
-            key={item.id}
-            onClick={() => handleItemClick(item.nav)}
-            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
-          >
-            {item.text}
-          </li>
-        ))}
-       
-      </ul>
+  
       <select
-          name="currency"
-          className="bg-cyan-500 border border-white text-white px-4 rounded cursor-pointer"
-          onChange={currencyHandler}
-          value={currentCurrency}
-     
-        >
-            {availableCurrency.map((item,index)=> <option key={index} value={item}>{item}</option>)}
-        </select>
+        name="currency"
+        className="bg-transparent border border-white text-white px-4 rounded cursor-pointer" // Set bg-transparent for the select
+        onChange={currencyHandler}
+        value={currentCurrency}
+      >
+        {availableCurrency.map((item, index) => (
+          <option key={index} value={item}>{item}</option>
+        ))}
+      </select>
     </div>
+  </div>
+  
+  
 
   );
 };
